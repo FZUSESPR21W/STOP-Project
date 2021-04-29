c64fa7b3f61909e1c4a6c2fe89543d1aa3e61758
GaryChan029 <garychan929@ymail.com>
2021-04-29 20:16:46 +0800

    [Updated] 完成基本情况-用户登录记录

--- frontend/src/views/admin/basic-info/base-state.vue
+++ frontend/src/views/admin/basic-info/base-state.vue
@@ -2,13 +2,23 @@
   <!-- 基本情况页面容器开始 -->
   <div class="base-state-container">
     <!-- 用户登录记录开始 -->
-    <div class="user-login-log-containerr">
-
+    <div class="user-login-log-container">
+      <span class="title">用户登录记录</span>
+      <!-- 用户登录记录表格，数据来源：userLoginData -->
+      <el-table :data="userLogin.userLoginData" style="width: 100%" stripe>
+        <!-- 日期字段 -->
+        <el-table-column prop="date" label="日期" width="180" />
+        <!-- 姓名字段 -->
+        <el-table-column prop="name" label="姓名" width="180" />
+        <!-- 登录地字段 -->
+        <el-table-column prop="space" label="地址" />
+      </el-table>
+      <!-- 用户登录记录表格结束 -->
     </div>
     <!-- 用户登录记录结束 -->
     <!-- 数据可视化(echarts图表）开始 -->
-    <div class="echarts-container">
-
+    <div class="graph-container">
+      <span class="title">数据可视化</span>
     </div>
     <!-- 数据可视化(echarts图表）结束 -->
   </div>
@@ -17,19 +27,66 @@
 
 <script>
 export default {
-  name: "base-state"
+  name: "base-state",
+  data() {
+    return {
+      //用户登录记录对象
+      userLogin: {
+        //用户登录数据
+        userLoginData: [],
+        //页面下标
+        page: 0,
+        //每页限制条数
+        limit: 5
+      },
+      //数据可视化(echarts图表）对象
+      graph: {
+
+      }
+    }
+  },
+  beforeMount() {
+    //填充假数据
+    for(let i = 0 ; i < 5 ; i ++){
+        this.userLogin.userLoginData.push(
+            {
+              name: '王小虎',
+              date: '2016-05-01',
+              space: '上海市普陀区金沙江路 1518 弄'
+            }
+        )
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.base-state-contianer {
+.base-state-container {
   display: flex;
   flex-direction: column;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+
 }
 
-.user-login-log-contianer {
+.user-login-log-container, .graph-container{
   flex: 0 0 50%;
+  padding-top: 20px;
+  padding-left: 50px;
+
+
+  .title {
+    font-family: "Microsoft YaHei UI Bold", "Microsoft YaHei UI Regular", "Microsoft YaHei UI", sans-serif;
+    font-weight: 700;
+    font-size: 20px;
+    text-decoration: underline;
+  }
+
 }
+
+
+
 
 
 </style>
