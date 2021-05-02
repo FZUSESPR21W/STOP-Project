9526ed631aea6ea3401c8727ef0609facb4393d7
Liangdayi <1113901523@qq.com>
2021-05-02 23:44:59 +0800

    [Fixed] 接受参数改成json

--- java/src/main/java/com/fzu/stop/controller/AdminController.java
+++ java/src/main/java/com/fzu/stop/controller/AdminController.java
@@ -1,28 +1,41 @@
 package com.fzu.stop.controller;
 
 import cn.dev33.satoken.stp.StpUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.fzu.stop.Util.ResponseUtil;
 import com.fzu.stop.pojo.AdminDO;
 import com.fzu.stop.pojo.ResponseDTO;
 import com.fzu.stop.service.AdminService;
-import org.jetbrains.annotations.NotNull;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 
 /**
  * @author 梁达毅
  */
 @Controller
+@Validated
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
     AdminService adminService;
+    @Operation(description = "登录验证")
+    @ApiResponses({
+            @ApiResponse(responseCode ="200",description = "成功"),
+            @ApiResponse(responseCode = "400",description = "请求参数没填好")
+    })
     @PostMapping("/login")
     @ResponseBody
-    public ResponseDTO login(@NotNull String username, @NotNull String password){
+    public ResponseDTO login(@RequestBody AdminDO adminDO){
+        String username=adminDO.getUsername();
+        String password=adminDO.getPassword();
         AdminDO admin = adminService.login(username, password);
         if(admin!=null){
             StpUtil.setLoginId(admin.getId());
@@ -31,10 +44,15 @@ public class AdminController {
             return ResponseUtil.getFailResponse("登陆失败",new HashMap<>());
         }
     }
-    @GetMapping("/visit")
-    @ResponseBody
-    public String visit(){
-        return (String) StpUtil.getLoginId();
-    }
 
+    @Operation(description = "登出账号")
+    @ApiResponses({
+            @ApiResponse(responseCode ="200",description = "成功")
+    })
+    @PostMapping("/logout")
+    @ResponseBody
+    public ResponseDTO logout(){
+        StpUtil.logout();
+        return ResponseUtil.getSuccessResponse("登出成功",new HashMap<>());
+    }
 }
