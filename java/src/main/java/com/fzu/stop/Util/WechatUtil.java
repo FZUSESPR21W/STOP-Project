1a68ba60fe5c1fb1893d3eb0dd48a6fc56f74971
Liangdayi <1113901523@qq.com>
2021-05-04 19:15:10 +0800

    [Added]获取用户登录记录

--- java/src/main/java/com/fzu/stop/Util/WechatUtil.java
+++ java/src/main/java/com/fzu/stop/Util/WechatUtil.java
@@ -16,6 +16,7 @@ public class WechatUtil {
     private static final String APPID = "wxd26114514c386951";
     private static final String SECRET = "1b8c5d5017ab11f4f4f24e10ea5b1475";
     private static final String AUTHORIZATION_CODE = "authorization_code";
+    private static final String GRANT_TYPE="client_credential";
 
     public static JSONObject getCode2Session(String code) throws IOException {
         String requestUrl = "https://api.weixin.qq.com/sns/jscode2session";
@@ -31,7 +32,49 @@ public class WechatUtil {
                 .build();
         Call call = okHttpClient.newCall(request);
         Response response=call.execute();
-        JSONObject jsonObject= (JSONObject) JSON.parse(response.body().toString());
+        JSONObject jsonObject= (JSONObject) JSON.parse(response.body().string());
         return  jsonObject;
     }
+
+    public static JSONObject getAccessToken() throws IOException {
+        String requestUrl ="https://api.weixin.qq.com/cgi-bin/token";
+        OkHttpClient okHttpClient=new OkHttpClient();
+        HttpUrl.Builder httpBuilder=HttpUrl.parse(requestUrl).newBuilder();
+        httpBuilder.addQueryParameter("grant_type",GRANT_TYPE);
+        httpBuilder.addQueryParameter("appid",APPID);
+        httpBuilder.addQueryParameter("secret",SECRET);
+        Request request=new Request.Builder()
+                .url(httpBuilder.build())
+                .build();
+        Response response = okHttpClient.newCall(request).execute();
+        JSONObject jsonObject= (JSONObject) JSON.parse(response.body().string());
+        return jsonObject;
+    }
+
+    public static JSONObject getUserPortrait(String begin_date,String end_date) throws IOException {
+        String accessToken =  WechatUtil.getAccessToken().getString("access_token");
+        String requestUrl="https://api.weixin.qq.com/datacube/getweanalysisappiduserportrait";
+        OkHttpClient okHttpClient=new OkHttpClient();
+        HttpUrl.Builder httpBuilder=HttpUrl.parse(requestUrl).newBuilder();
+        httpBuilder.addQueryParameter("access_token",accessToken);
+        JSONObject data=new JSONObject();
+        data.put("begin_date",begin_date);
+        data.put("end_date",end_date);
+        /*需要将数据格式设置为application/json,否则会出现47001----data format error rid: 6084cf6b-7c22f070-136ea237*/
+        MediaType mediaType=MediaType.parse("application/json;charset=utf-8");
+        RequestBody body=RequestBody.create(mediaType,String.valueOf(data));
+        Request request=new Request.Builder()
+                .url(httpBuilder    .build())
+                .post(body)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        Response response=call.execute();
+        JSONObject jsonObject= (JSONObject) JSON.parse(response.body().string());
+        return jsonObject;
+    }
+
+
+
+
+
 }
--- java/src/main/java/com/fzu/stop/controller/StatisticsController.java
+++ java/src/main/java/com/fzu/stop/controller/StatisticsController.java
@@ -1,6 +1,8 @@
 package com.fzu.stop.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fzu.stop.Util.ResponseUtil;
+import com.fzu.stop.Util.WechatUtil;
 import com.fzu.stop.pojo.ParkingSituationDO;
 import com.fzu.stop.pojo.ResponseDTO;
 import com.fzu.stop.service.StatisticsService;
@@ -10,16 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.SimpleFormatter;
+import java.io.IOException;
+import java.util.*;
+
 
 /**
- * @author 宋家锐
+ * @author 宋家锐、梁达毅
  */
 @Controller
 @RequestMapping("/api")
@@ -48,6 +51,27 @@ public class StatisticsController {
             return ResponseUtil.getSuccessResponse("获取成功", data);
         }
         return ResponseUtil.getFailResponse("获取失败", new HashMap<>(16));
+    }
+
+    @PostMapping("/get_login_list")
+    @ResponseBody
+    public ResponseDTO getLoginList(Integer delta) throws IOException {
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMdd");
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DAY_OF_YEAR,-1);
+        //允许设置的最大值为昨日
+        Date cur_date=calendar.getTime();
+        //开始日期与结束日期相差的天数限定为0/6/29，分别表示查询最近1/7/30天数据,即delta只能为0、6、29
+        calendar.add(Calendar.DAY_OF_YEAR,-delta);
+        Date past_date=calendar.getTime();
+        String begin_date=simpleDateFormat.format(past_date);
+        String end_date=simpleDateFormat.format(cur_date);
+        JSONObject jsonObject=WechatUtil.getUserPortrait(begin_date,end_date);
+        Map<String,Object> data =new HashMap<>();
+        data.put("data",jsonObject);
+        return ResponseUtil.getSuccessResponse("获取成功",data);
+
     }
 
 }
