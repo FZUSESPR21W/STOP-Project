be354974e4a1c869b7d996461041406052ba1ed9
Savona <905899183@qq.com>
2021-05-05 00:41:39 +0800

    [Update]推荐地点框及部分动画

--- mini-app/pages/parking/index.vue
+++ mini-app/pages/parking/index.vue
@@ -1,7 +1,22 @@
 <template>
-	<view>
-		<ParkingMap/>
-		我是我要停车页面
+	<view >
+		<ParkingMap
+			@hideList="hideList"
+		/>
+		<view class="location-box" :class="{hideListStart:status0,showListStart:status1}"
+		 :style="{height:locationBoxHeight,Top:locationBoxTop}"
+			>
+			<view style="width: 100%;padding-top:10px; padding-bottom: 4px;" @click="showList"><view class="exhale-bar" v-show="showExhaleBar"></view></view>
+			<view class="input-box-border">
+				<input
+					type="text"
+					class="input-box"
+					placeholder="查询想要停车的地点"
+					maxlength="40"
+				/>
+			</view>
+			<view class="place-list" v-show="showPlaceList">这里推荐停车地点</view>
+		</view>
 	</view>
 </template>
 
@@ -9,11 +24,121 @@
 import ParkingMap from './parking-map/index.vue';
 export default{
 	name:'Parking',
+	data(){
+		return{
+			status0:false,
+			status1:false,
+			status2:false,
+			showExhaleBar:false,
+			showPlaceList:true,
+			locationBoxHeight:'44vh',
+			locationBoxTop:'56vh',
+		}
+	},
 	components:{
 		ParkingMap,
 	},
+	methods:{
+		
+		//显示推荐地点列表
+		showList(){
+			this.changeLocationBox(1);
+		},
+		
+		//收到map组件传回来的值，显示呼出条、隐藏地点列表
+		hideList(res){
+			this.changeLocationBox(0);
+		},
+		
+		//更改地点框状态，0：底栏(默认)；1：半屏；2：全屏；
+		changeLocationBox(status){
+			var _this=this;
+			if(status === 0){
+				_this.showExhaleBar=true;
+				_this.showPlaceList=false;
+				_this.status0=true;
+				_this.status1=false;
+				_this.locationBoxHeight='14vh';
+				_this.locationBoxTop='86vh';
+			}
+			else if(status === 1){
+				_this.status0=false;
+				_this.status1=true;
+				_this.showExhaleBar=false;
+				_this.showPlaceList=true; 
+				_this.locationBoxHeight='44vh';
+				_this.locationBoxTop='56vh';
+			}
+			else if(status === 2){
+				console.log('全屏尚未完成');
+			}
+		}
+	}
 }
 </script>
 
-<style>
+<style scoped>
+	/* 隐藏推荐地点框的动画 */
+	@keyframes hideList {
+	  from {height: 44vh;top:56vh}
+	  to {height: 14vh;top: 86vh;}
+	}
+	/* 显示推荐地点框的动画 */
+	@keyframes showList {
+	  from {height: 14vh;top:86vh}
+	  to {height: 44vh;top: 56vh;}
+	}
+	/* 地点框 */
+	.location-box{
+		background-color: white;
+		width: 100%;
+		border-radius: 10px 10px 0 0;
+		position: fixed;
+		border-top: 1px solid rgba(191, 191, 191, 0.4);
+	}
+	/* 开始隐藏动画 */
+	.hideListStart{
+		 animation-name: hideList;
+		 animation-duration: 0.4s;
+	}
+	/* 开始展示动画 */
+	.showListStart{
+		animation-name: showList;
+		animation-duration: 0.4s;
+	}
+	/* 呼出条 */
+	.exhale-bar{
+		width: 20%;
+		height: 1vh;
+		margin: 0 auto;
+		/* margin-top: 10px; */
+		border-radius: 50px;
+		background-color: rgba(89,89,89,0.7);
+	}
+	/* 输入框外框 */
+	.input-box-border{
+		height: fit-content;
+		border: 1px solid black;
+		background-color: #f5f5f5;
+		margin: 0 auto;
+		margin-top: 10px;
+		width: 86%;
+		padding: 4px;
+		border-radius: 10px ;
+	}
+	/* 输入框 */
+	.input-box{
+		width: 100%;
+		height: 30px;
+		background-color: #f5f5f5;
+		border: 1px solid red;
+		display: block;
+		font-size: 18px;
+	}
+	/* 推荐地点列表 */
+	.place-list{
+		margin-top: 10px;
+		background-color: white;
+		height: 100%;
+	}
 </style>
--- mini-app/pages/parking/parking-map/index.vue
+++ mini-app/pages/parking/parking-map/index.vue
@@ -1,12 +1,99 @@
 <template>
-	<view>我的停车地图组件</view>
+	<view>
+		<map 
+			:latitude="latitude" 
+			:longitude="longitude" 
+			id="parkingMap" 
+			:markers="covers"
+			:controls="control"
+			:show-location="true"
+			@markertap="clickMarker"
+			@regionchange="clickMap"
+			style="width: 100%;"
+			:style="{height:mapHeight}">
+		</map>
+	</view>
 </template>
 
 <script>
 export default{
 	name:'ParkingMap',
+	data(){
+		return{
+			latitude:'',
+			longitude:'',
+			covers:[],
+			mapHeight:'90vh',
+			control:[],
+		}
+	},
+	methods: {
+		
+		//地图点击事件
+		clickMap(res){
+			var _this=this;
+			_this.mapHeight='90vh';
+			_this.$emit('hideList',true);
+		},
+		
+		//标记点击事件
+		clickMarker(res){
+			console.log(res);
+		},
+		
+		//获取用户位置
+		getLocation(){
+			var _this=this;
+			uni.showLoading({
+				title:'正在获取定位',
+			});
+			uni.getLocation({
+				type:'gcj02',
+				timeout:'2000',
+				
+				success(res) {
+					uni.hideLoading();
+					_this.longitude=res.longitude;
+					_this.latitude=res.latitude;
+					console.log(res);
+					console.log(_this.longitude);
+					console.log(_this.latitude);
+					_this.covers[0]={
+						latitude:res.latitude+0.00001,
+						longitude:res.longitude+0.00001,
+						width:40,
+						height:40,
+						iconPath:'../../../static/map-icon/我的位置.png'
+					};
+					_this.control[0]={
+						iconPath:'../../../static/map-icon/我的位置.png',
+						clickable:true,
+						position:{
+							width:40,
+							height:30,
+						},
+					};
+				},
+				fail(err) {
+					uni.hideLoading();
+					uni.showModal({
+						title:'提示',
+						content:'位置信息获取失败（请检查定位功能是否打开）',
+						showCancel:false
+					})
+				}
+			})
+		},
+	},
+	created() {
+		var _this=this;
+		_this.getLocation();
+	}
 }
 </script>
 
-<style>
+<style scoped>
+#parkingMap{
+	width: 100%;
+}
 </style>
