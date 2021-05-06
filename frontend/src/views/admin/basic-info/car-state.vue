a42b353a047346bae3a1294527bdce5fbe672d6d
GaryChan029 <garychan929@ymail.com>
2021-05-06 08:44:13 +0800

    [Updated] 完成车辆信息页面

--- frontend/src/views/admin/basic-info/car-state.vue
+++ frontend/src/views/admin/basic-info/car-state.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="car-state-contianer">
+  <div class="car-state-container">
     <div id="car" class="graph"/>
   </div>
 </template>
@@ -11,14 +11,38 @@ export default {
     return {
       graph: {
         car: {
-
+          xAxis: {
+            type: 'category',
+            data: ['一区', '二区', '三区', '四区', '五区', '研究生公寓',
+              '紫荆', '玫瑰', '丁香', '京元', '西三', '西二', '西一', '中楼',
+              '东一', '东二', '文楼']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            data: [500, 420, 490, 380, 356, 343, 332,
+              330, 327, 304, 278, 267, 213, 142, 102, 102, 55, 55],
+            type: 'bar',
+            itemStyle: {
+              normal: {
+                color: function (params) {
+                  return ['#7CB5EC', '#434348', '#90ED7D', '#F7A35C',
+                    '#8085E9', '#F15C80', '#E4D354', '#2B908F', '#F45B5B'
+                    , '#91E8E1', '#7CB5EC', '#434348', '#90ED7D', '#F7A35C',
+                    '#8085E9', '#F15C80', '#E4D354', '#2B908F', '#F45B5B'
+                    , '#91E8E1'][params.dataIndex]
+                }
+              }
+            }
+          }]
         }
       }
     }
   },
   beforeMount() {
     // 获取停车情况
-    this.getCarState()
+    // this.getCarState()
   },
   mounted() {
     // 绘制车辆信息图表
@@ -26,15 +50,20 @@ export default {
   },
   methods: {
     // 获取停车情况
-    getCarState() {},
+    getCarState() {
+    },
     // 绘制车辆信息图表
-    paintCarState() {}
+    paintCarState() {
+      let domContainer = document.getElementById('car')
+      let chart = this.$echarts.init(domContainer)
+      chart.setOption(chart)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.car-state-contianer {
+.car-state-container {
   height: 100%;
   width: 100%;
 
