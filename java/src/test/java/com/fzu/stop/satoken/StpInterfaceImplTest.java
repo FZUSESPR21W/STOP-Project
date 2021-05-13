2f504a7c3d542ec0e039b9262426a08fd2c5e4d8
Liangdayi <1113901523@qq.com>
2021-05-13 16:49:08 +0800

    [Added]StpInterface接口测试

--- java/src/test/java/com/fzu/stop/satoken/StpInterfaceImplTest.java
+++ java/src/test/java/com/fzu/stop/satoken/StpInterfaceImplTest.java
@@ -2,20 +2,24 @@ package com.fzu.stop.satoken;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class StpInterfaceImplTest {
     @Autowired
     StpInterfaceImpl stpInterface;
     @Test
     void getPermissionList() {
-        List<String> list = stpInterface.getPermissionList(1, null);
-        for (String s : list) {
-            System.out.println(s);
+        List<String> list = stpInterface.getPermissionList("1", null);
+        if(list!=null){
+            for (String s : list) {
+                System.out.println(s);
+            }
         }
+
     }
 
     @Test
