0ea529ad9ab08f4a41ff9ec16dd4376145914310
6586744 <cxsjr@foxmail.com>
2021-05-14 16:55:23 +0800

    [Added] FeedBcak Test

---
+++ java/src/test/java/com/fzu/stop/service/FeedbackServiceTest.java
@@ -0,0 +1,62 @@
+package com.fzu.stop.service;
+
+import com.fzu.stop.pojo.FeedbackDO;
+import com.fzu.stop.util.WechatUtil;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootTest
+class FeedbackServiceTest {
+    @Autowired
+    FeedbackService feedbackService;
+
+    List<FeedbackDO> feedbackDOList;
+
+
+    @BeforeEach
+    void setUp() {
+        feedbackDOList = new ArrayList<>();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getFeedbackList() {
+        feedbackDOList = feedbackService.getFeedbackList(1,5);
+        Assertions.assertEquals(5,feedbackDOList.size());
+    }
+
+    @Test
+    void getFeedback() {
+        FeedbackDO feedbackDO;
+        feedbackDO = (FeedbackDO) feedbackService.getFeedback(2);
+        Assertions.assertEquals("李四",feedbackDO.getUsername());
+    }
+
+    @Test
+    void updateFeedback() {
+        int flag = feedbackService.updateFeedback(2,2);
+        Assertions.assertEquals(1,flag);
+    }
+
+    @Test
+    void addFeedback() {
+        FeedbackDO feedbackDO = new FeedbackDO();
+        feedbackDO.setContent("test");
+        feedbackDO.setUsername("李明");
+        feedbackDO.setOpenId("test123");
+//        Assertions.assertTrue(feedbackService.addFeedback(feedbackDO));
+    }
+}
